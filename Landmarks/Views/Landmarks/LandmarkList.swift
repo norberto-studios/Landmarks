1832e751294a66b9d667ef7e5d6656f8cc84b1bd
NorbertoStudios <norbertostudios@gmail.com>
2021-08-15 00:12:00 -0400

    Added Favorite

---
+++ Landmarks/Views/Landmarks/LandmarkList.swift
@@ -0,0 +1,41 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Norberto Fernandez on 8/12/21.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    @EnvironmentObject var modelData: ModelData
+    @State private var showFavoriteOnly = false
+    
+    var filteredLanmarks: [Landmark] {
+        modelData.landmarks.filter{ landmark in
+            (!showFavoriteOnly || landmark.isFavorite)
+        }
+    }
+    
+    var body: some View {
+        NavigationView {
+            List {
+                Toggle(isOn: $showFavoriteOnly) {
+                    Text("Favorite only")
+                }
+                ForEach(filteredLanmarks) { landmark in
+                    NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                        LandmarkRow(landmark: landmark)
+                    }
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
